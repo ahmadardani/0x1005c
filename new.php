f9dd7482810bc4017c78443fa93d3acf81b0f10e
ahmadardani <ahmadardani2011@gmail.com>
2025-02-11 08:52:12 +0800

    cleaned up some unused stuff and tweaked the css

--- new.php
+++ new.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bookmarks</title>
+    <title>New Category</title>
     <link rel="icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="style.css"> <!-- Link external CSS file -->
 </head>
@@ -11,13 +11,16 @@
 
     <div class="wrapper">
         <div class="header">
-            <span>Bookmarks</span>
-            <a href="new.php"><button>Add Category</button></a> 
+            <span>Bookmarks</span> 
         </div>
         
         <div class="container">
             <div class="content">
-                <p>Bookmark all of your lovely sites!</p>
+                <p>Create a new category based on your interest!</p>
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+  Category Name: <input type="text" name="category_name"><br><br>
+  <input type="submit" name="submit" value="Add Category">
+</form>
                 <p><?php echo "null"; ?></p>
             </div>
         </div>
